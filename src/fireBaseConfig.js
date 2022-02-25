e482e78853507b3378527ec96f43abd6ae4f5418
Jean <jeanncavalcante@gmail.com>
2022-02-24 21:29:09 -0300

    Ciando integração com firebase

---
+++ src/fireBaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCZKgX2_uFAssSj8zfAnjfzoupEjqH0vPc",
+  authDomain: "wttclone.firebaseapp.com",
+  projectId: "wttclone",
+  storageBucket: "wttclone.appspot.com",
+  messagingSenderId: "424152453432",
+  appId: "1:424152453432:web:380a68f9e2783a1a90f1cd",
+  measurementId: "G-G44YXNDLW3",
+};
