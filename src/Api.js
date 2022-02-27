f27318041f91b89418de6c662ee75c254c136450
Jean <jeanncavalcante@gmail.com>
2022-02-27 19:28:25 -0300

    correçção do fireBaseConfig

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firebase-auth";
 import "firebase/firebase-firestore";
 
-import firebaseConfig from "./firebaseConfig";
+import firebaseConfig from "./fireBaseConfig";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
